f782f00b0638c90e4b8ab60324da78955f237fb3
Brian Davis <bscottdavis@gmail.com>
2022-03-30 21:53:14 -0700

    Create User

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { signup } from '../firebase/auth';
+
 
 function Signup() {
+  const { register, handleSubmit, reset} = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      await signup(data);
+      reset();
+    } catch (error){
+      console.log(error);
+  }
+
   return (
     <div className="login-container">
       <div className="ui card login-card">
         <div className="content">
-          <form className="ui form">
+          <form className="ui form" onSubmit={handleSubmit(onSubmit)}>
             <div className="two fields">
               <div className="field">
                 <label>
@@ -15,26 +27,27 @@ function Signup() {
                     type="text"
                     name="firstName"
                     placeholder="First Name"
+                    ref={register}
                   />
                 </label>
               </div>
               <div className="field">
                 <label>
                   Last Name
-                  <input type="text" name="lastName" placeholder="Last Name" />
+                  <input type="text" name="lastName" placeholder="Last Name" ref={register}/>
                 </label>
               </div>
             </div>
             <div className="field">
               <label>
                 Email
-                <input type="email" name="email" placeholder="Email" />
+                <input type="email" name="email" placeholder="Email" ref={register} />
               </label>
             </div>
             <div className="field">
               <label>
                 Password
-                <input type="password" name="password" placeholder="Password" />
+                <input type="password" name="password" placeholder="Password" ref={register} />
               </label>
             </div>
             <button className="ui primary button login" type="submit">
