b754702f1fe5168b65e80583642efb64aa70e398
Brian Davis <bscottdavis@gmail.com>
2022-04-02 14:12:52 -0700

    updateing user docs

---
+++ src/firebase/user.js
@@ -0,0 +1,25 @@
+import { firestore } from './config';
+
+export const createUserDocument = async (user) => {
+    const docRef = firestore.doc(`/users/${user.uid}`);
+
+    const userProfile = {
+        uid: user.uid,
+        email: user.email,
+        name: user.displayName,
+        address: '',
+        city: '',
+        state: '',
+        phone: '',
+        speciality: '',
+        ip: '',
+    };
+
+
+    return docRef.set(userProfile);
+}
+
+export const updateUserDocument = async (user) => {
+    const docRef = firestore.doc(`/users/${user.uid}`);
+    return docRef.update(user);
+}
