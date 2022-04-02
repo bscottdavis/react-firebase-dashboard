82b49e74a44b28425ee3ced3d79b7a0067542481
Brian Davis <bscottdavis@gmail.com>
2022-04-02 15:55:18 -0700

    temp

---
+++ src/firebase/ProfileImage.js
@@ -0,0 +1,40 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { uploadImage, getDownloadUrl } from './user';
+
+export const ProfileImage = ({ id }) => {
+  const fileInput = useRef(null);
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    getDownloadUrl(id).then((url) => !!url && setImageUrl(url));
+  }, [id]);
+
+  const fileChange = async (files) => {
+    const ref = await uploadImage(id, files[0]);
+    const downloadUrl = await ref.getDownloadURL();
+    setImageUrl(downloadUrl);
+  };
+
+  return (
+    <div className="four wide column profile-image">
+      <img
+        className="ui image"
+        src={imageUrl || '/profile-placeholder.png'}
+        alt="profile"
+      />
+      <input
+        className="file-input"
+        type="file"
+        accept=".png,.jpg"
+        ref={fileInput}
+        onChange={(e) => fileChange(e.target.files)}
+      />
+      <button
+        className="ui grey button upload-button"
+        onClick={() => fileInput.current.click()}
+      >
+        Upload Photo
+      </button>
+    </div>
+  );
+};
